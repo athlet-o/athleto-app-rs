11b3e6e6d796c3d123c1cbc321673fa4a0e79d31
Alexander Mills <alexander.d.mills@gmail.com>
2026-07-16 19:47:39 -0500

    claude autosave 2026-07-17T00:47:39Z

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Development environment for athleto-app-rs";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = import ./.nix/dev-shell.nix { inherit pkgs; };
+        }
+      );
+    };
+}
